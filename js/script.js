fe6372a263e9a0c5c0b621e906731f32c3f9d048
bandreae <72764934+bandreae@users.noreply.github.com>
2021-06-07 19:01:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the password input
@@ -10,3 +22,6 @@ function writePassword() {
         "lowerCase": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
         "endPassword": []  
 }
+    
+    // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
